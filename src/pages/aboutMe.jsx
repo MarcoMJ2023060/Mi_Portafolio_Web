cf3c85767f6599e6a27238399bf11436018f8b82
MarcoMJ2023060 <marcojosepro2007@gmail.com>
2025-07-17 11:23:24 -0600

    PORTAFOLIO WEB:mejoras en about me

--- src/pages/aboutMe.jsx
+++ src/pages/aboutMe.jsx
@@ -25,19 +25,61 @@ const AboutMe = () => {
       window.removeEventListener("resize", updateBackground);
     };
   }, []);
-  return (
+  return (  
     <>
       <section id="aboutMe" className="sm:p-10 lg:p-20 p-5 aboutMe">
         <div className="container mx-auto mt-[1rem]">
           <div className="sm:grid grid-cols-2 max-[1250px]:grid-cols-1">
             <div className="sm:p-10 p-5 bg-white sm:me-5 mb-10 rounded-md shadow-md h-[auto] ">
-              <h3 className="text-2xl mb-5 font-bold">Un poco acerca mí</h3>
+              <h3 className="text-2xl mb-5 font-bold">Frase</h3>
               <p className="leading-10 text-gray-500 text-2xl max-[750px]:text-[17px]">
-                ¡Hola! Soy Marco Jose Bolaños, actualmente tengo 18 años de edad
-                y soy un apasionado por la tecnología. <br /> <br /> Desde
-                pequeño, me ha fascinado aprender constantemente y descubrir
-                cómo funcionan las cosas. Me considero un joven entusiasta,
-                siempre buscando mejorar día a día.
+                <span className="font-bold text-gray-800">
+                  "Ningún sueño es tan grande, y ningún soñador tan pequeño"
+                </span>
+                <br />
+                <br /> 
+              </p>
+              <h3 className="text-2xl mb-5 font-bold">¿Qué significa?</h3>
+              <p className="leading-10 text-gray-500 text-2xl max-[750px]:text-[17px]">
+                 <span className="font-bold text-gray-800">
+                  Significa que no existe ningún sueño grande que sea imposible de conseguir, ningún soñador pequeño, nos habla acerca de que todos tenemos la capacidad de conseguir los sueños "grandes".
+                </span>
+                <br />
+                <br /> 
+              </p>
+              <h3 className="text-2xl mb-5 font-bold text-gray-800">Acerca de mi</h3>
+              <p className="leading-10 text-gray-500 text-2xl max-[750px]:text-[17px]">
+                <span className="font-bold text-gray-800">
+                  ¡Hola! Soy Marco Jose Bolaños, desarrollador junior actualmente
+                  tengo 18 años de edad, soy un apasionado por la tecnología.
+                </span>
+                <br />
+                <br />
+                <span className="font-bold text-gray-800">
+                  Desde pequeño he sido muy curioso y me ha fascinado aprender constantemente y descubrir cómo funcionan las cosas. Me considero un joven entusiasta, siempre buscando mejorar día a día.
+                </span>
+                <br /> <br />
+                <span className="font-bold text-gray-800">
+                  Actualmente curso perito en informática en KINAL, combino los conocimientos adquiridos durante estos 2 años y siete meses de carrera.
+                </span>
+                <br /> <br />
+                <span className="font-bold text-gray-800">
+                  Soy estudiante de Perito en Computación en el Centro Educativo Técnico Laboral Kinal
+                </span>
+                <br /> <br />
+                <span className="font-bold text-gray-800">
+                  Experiencia en React y Node JS
+                </span>
+                <br /> <br />
+                <span className="font-bold text-gray-800">
+                  Experiencia en accesibilidad web y en experiencia de usuario (UX)
+                </span>
+                <br /> <br />
+                <span className="font-bold text-gray-800">
+                  Mentalidad positiva, minimalista, sobre todo escalable.
+                </span>
+                <br /> <br />
+                <span className="font-bold text-gray-800">Años Desarrollando: 2 años 7 meses</span>
               </p>
             </div>
           </div>
