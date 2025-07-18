077c1c76abf73fc781ee4178636ea96a37817a4a
MarcoMJ2023060 <marcojosepro2007@gmail.com>
2025-07-18 08:13:15 -0600

    PORTAFOLIO WEB:Se elimino el path de la ruta acerca de mi que ya no se esta usando

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "../pages/homePage";
-import AboutMe from "../pages/aboutMe";
 import Proyects from "../pages/proyects";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
-  },
-  {
-    path: "/aboutMe",
-    element: <AboutMe />,
   },
   {
     path: "/proyects",
